aa9df6226c51151b3ee051506984d2d5ef48bc55
DrKToN <everton.drk@gmail.com>
2025-09-20 13:38:59 -0300

    Modificado o texto do app

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const port = process.env.PORT || 3000;
 
 // Cria uma rota principal ("/") que responde com uma mensagem
 app.get("/", (req, res) => {
-    res.send(
-        "<h1>Olá, Mundo!</h1><p>Meu primeiro app Node.js rodando no Azure!</p>"
-    );
+    res.send("<h1>Olá, Mundo!</h1><p>Meu webapp Node.js rodando no Azure!</p>");
 });
 
 // Inicia o servidor e o faz "escutar" na porta definida
